98774b3af57d63af0fc65358af114eab8b18151f
Mannshah123 <mannrshah.1704@gmail.com>
2025-12-04 20:46:32 -0600

    added scriptable objects

---
+++ Assets/ScriptableObjects/ScriptableObject Scripts/Symptons.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Symptons", menuName = "ScriptableObjects/Symptons")]
+public class Symptons : ScriptableObject
+{
+    public bool fever;
+    public bool dizziness;
+    public bool MusclePain;
+    public bool cough;
+}
