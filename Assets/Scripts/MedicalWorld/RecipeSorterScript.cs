8be700d6ef0bd40bfd855a2683a1466f77c9dd18
Mannshah123 <mannrshah.1704@gmail.com>
2026-01-03 14:14:47 -0600

    added a recipe sorter

---
+++ Assets/Scripts/MedicalWorld/RecipeSorterScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+public class RecipeSorterScript : MonoBehaviour
+{
+
+    // public GameObject syringeRecipePanel;
+    // public GameObject medicineRecipePanel;
+
+
+    // public GameObject descriptionPrefab;
+    // public Symptoms[] syringeSymptoms;
+    // public Symptoms[] medicineSymptoms;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // for(int i =0; i< syringeSymptoms.Length; i++){
+        //     GameObject descriptionInstance = Instantiate(descriptionPrefab);
+        //     descriptionInstance.transform.SetParent(syringeRecipePanel.transform, false);
+        //     TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
+        //     descriptionText.text = syringeSymptoms[i].symptomName + ": " + syringeSymptoms[i].treatmentDescription;
+        // }
+
+        // for(int j =0; j< medicineSymptoms.Length; j++){
+        //     GameObject descriptionInstance = Instantiate(descriptionPrefab);
+        //     descriptionInstance.transform.SetParent(medicineRecipePanel.transform, false);
+        //     TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
+        //     descriptionText.text = medicineSymptoms[j].symptomName + ": " + medicineSymptoms[j].treatmentDescription;
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
