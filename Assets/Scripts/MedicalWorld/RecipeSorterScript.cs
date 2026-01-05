c2332b0ac5c435243b67d744147e4741af9ddec1
Mannshah123 <mannrshah.1704@gmail.com>
2026-01-05 17:04:44 -0600

    added basic description

--- Assets/Scripts/MedicalWorld/RecipeSorterScript.cs
+++ Assets/Scripts/MedicalWorld/RecipeSorterScript.cs
@@ -6,29 +6,29 @@ using TMPro;
 public class RecipeSorterScript : MonoBehaviour
 {
 
-    // public GameObject syringeRecipePanel;
-    // public GameObject medicineRecipePanel;
+    public GameObject syringeRecipePanel;
+    public GameObject medicineRecipePanel;
 
 
-    // public GameObject descriptionPrefab;
-    // public Symptoms[] syringeSymptoms;
-    // public Symptoms[] medicineSymptoms;
+    public GameObject descriptionPrefab;
+    public Symptons[] syringeSymptoms;
+    public Symptons[] medicineSymptoms;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // for(int i =0; i< syringeSymptoms.Length; i++){
-        //     GameObject descriptionInstance = Instantiate(descriptionPrefab);
-        //     descriptionInstance.transform.SetParent(syringeRecipePanel.transform, false);
-        //     TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
-        //     descriptionText.text = syringeSymptoms[i].symptomName + ": " + syringeSymptoms[i].treatmentDescription;
-        // }
+        for(int i =0; i< syringeSymptoms.Length; i++){
+            GameObject descriptionInstance = Instantiate(descriptionPrefab);
+            descriptionInstance.transform.SetParent(syringeRecipePanel.transform, false);
+            descriptionInstance.GetComponentInChildren<TMP_Text>().text = syringeSymptoms[i].name;
+        }
 
-        // for(int j =0; j< medicineSymptoms.Length; j++){
-        //     GameObject descriptionInstance = Instantiate(descriptionPrefab);
-        //     descriptionInstance.transform.SetParent(medicineRecipePanel.transform, false);
-        //     TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
-        //     descriptionText.text = medicineSymptoms[j].symptomName + ": " + medicineSymptoms[j].treatmentDescription;
-        // }
+        for(int j =0; j< medicineSymptoms.Length; j++){
+            GameObject descriptionInstance = Instantiate(descriptionPrefab);
+            descriptionInstance.transform.SetParent(medicineRecipePanel.transform, false);
+            descriptionInstance.GetComponentInChildren<TMP_Text>().text = medicineSymptoms[j].name;
+            // TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
+            // descriptionText.text = medicineSymptoms[j].symptomName + ": " + medicineSymptoms[j].treatmentDescription;
+        }
     }
 
     // Update is called once per frame
