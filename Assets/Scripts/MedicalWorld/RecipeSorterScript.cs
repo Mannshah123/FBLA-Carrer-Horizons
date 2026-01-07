f1dfd60adf41dea2e6d887a38de7295926b5efc2
Mannshah123 <mannrshah.1704@gmail.com>
2026-01-06 23:20:14 -0600

    completed recipe book

--- Assets/Scripts/MedicalWorld/RecipeSorterScript.cs
+++ Assets/Scripts/MedicalWorld/RecipeSorterScript.cs
@@ -13,6 +13,8 @@ public class RecipeSorterScript : MonoBehaviour
     public GameObject descriptionPrefab;
     public Symptons[] syringeSymptoms;
     public Symptons[] medicineSymptoms;
+
+    private int totalSymptoms = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,14 +22,120 @@ public class RecipeSorterScript : MonoBehaviour
             GameObject descriptionInstance = Instantiate(descriptionPrefab);
             descriptionInstance.transform.SetParent(syringeRecipePanel.transform, false);
             descriptionInstance.GetComponentInChildren<TMP_Text>().text = syringeSymptoms[i].name;
+            Transform textTransform = descriptionInstance.transform.Find("Description");
+
+            if (syringeSymptoms[i].fever)
+        {
+            textTransform.GetComponent<TMP_Text>().text += "Fever";
+            totalSymptoms++;
+        }
+
+        if (syringeSymptoms[i].dizziness)
+        {
+            if (totalSymptoms > 0)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "     &     ";
+            }
+
+            textTransform.GetComponent<TMP_Text>().text += "Dizziness";
+            totalSymptoms++;
+        }
+
+        if (syringeSymptoms[i].MusclePain)
+        {
+            if (totalSymptoms > 1)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "\nMuscle Pain";
+            }
+            else if (totalSymptoms > 0)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                textTransform.GetComponent<TMP_Text>().text += "Muscle Pain";
+            }
+            else
+            {
+                textTransform.GetComponent<TMP_Text>().text += "Muscle Pain";
+            }
+            totalSymptoms++;
+        }
+
+        if (syringeSymptoms[i].cough)
+        {
+            if (totalSymptoms > 2)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                textTransform.GetComponent<TMP_Text>().text += "Cough";
+            }
+            else if (totalSymptoms > 1)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "\nCough";
+            }
+            else if (totalSymptoms > 0)
+            {
+                textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                textTransform.GetComponent<TMP_Text>().text += "Cough";
+            }
+            else
+            {
+                textTransform.GetComponent<TMP_Text>().text += "Cough\n";
+            }
+            totalSymptoms++;
+        }
+
+        // Reset totalSymptoms for the next symptom
+        totalSymptoms = 0;
         }
 
         for(int j =0; j< medicineSymptoms.Length; j++){
             GameObject descriptionInstance = Instantiate(descriptionPrefab);
             descriptionInstance.transform.SetParent(medicineRecipePanel.transform, false);
             descriptionInstance.GetComponentInChildren<TMP_Text>().text = medicineSymptoms[j].name;
+            Transform textTransform = descriptionInstance.transform.Find("Description");
+            if(medicineSymptoms[j].fever){
+                textTransform.GetComponent<TMP_Text>().text += "Fever";
+                totalSymptoms++;
+            }
+
+            if(medicineSymptoms[j].dizziness){
+                if(totalSymptoms > 0 ){
+                    textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                }
+
+                textTransform.GetComponent<TMP_Text>().text += "Dizziness";
+                totalSymptoms++;
+                
+            }
+
+            if(medicineSymptoms[j].MusclePain){
+                if(totalSymptoms > 1 ){
+                    textTransform.GetComponent<TMP_Text>().text += "\nMuscle Pain";
+                        totalSymptoms++;
+                }else if(totalSymptoms > 0){
+                        textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                    textTransform.GetComponent<TMP_Text>().text += "Muscle Pain";
+                    totalSymptoms++;
+                }else{
+                    textTransform.GetComponent<TMP_Text>().text += "Muscle Pain";
+                    totalSymptoms++;
+                }
+               
+            }
+            if(medicineSymptoms[j].cough){
+                if(totalSymptoms > 2){
+                     textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                     textTransform.GetComponent<TMP_Text>().text += "Cough";
+                }else if(totalSymptoms >1){
+                    textTransform.GetComponent<TMP_Text>().text += "\nCough";
+                }else if(totalSymptoms >0){
+                    textTransform.GetComponent<TMP_Text>().text += "     &     ";
+                    textTransform.GetComponent<TMP_Text>().text += "Cough";
+                }else{
+                textTransform.GetComponent<TMP_Text>().text += "Cough\n";
+                }
+            }
             // TMP_Text descriptionText = descriptionInstance.GetComponent<TMP_Text>();
             // descriptionText.text = medicineSymptoms[j].symptomName + ": " + medicineSymptoms[j].treatmentDescription;
+            totalSymptoms = 0;
         }
     }
 
